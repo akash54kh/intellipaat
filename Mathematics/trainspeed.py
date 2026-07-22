48d1f1b6c7abe7f09b3ff21d0a6ea727d9ce9544
Akash <akash9kh@rediffmail.com>
2026-07-22 16:33:37 +0530

    rVXxQGPgPHzs

---
+++ Mathematics/trainspeed.py
@@ -0,0 +1,24 @@
+import random, math
+
+s1 = s2 = 1
+
+while math.gcd(s1, s2) == 1:
+    s1 = random.randint(70, 100)
+    s2 = random.randint(70, 100)
+
+if s1 > s2:
+    temp = s2
+    s2 = s1
+    s1 = temp
+
+d = math.lcm(s1, s2)
+s = s2 - s1
+
+t1 = d / s1
+t2 = (s1 * t1) / s2
+t = int(t1 - t2)
+c = s1 * s2
+
+print('Distance: {}, Speed Increment: {}, Time Variation: {}'.format(d, s, t))
+print('Actual: {}km/hr, Virtual: {}km/hr'.format(s1, s2))
+print('A = 1, B = {}, C = -{}'.format(s, c))
