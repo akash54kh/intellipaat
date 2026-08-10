c6d5548cc7d64e1483cfa2e8bab8ee7cf3621995
Akash <akash9kh@rediffmail.com>
2026-08-10 07:21:54 +0530

    CcWsoIJdXxvPZI

--- Mathematics/trainspeed.py
+++ Mathematics/trainspeed.py
@@ -20,7 +20,7 @@ t = int(t1 - t2)
 c = s1 * s2
 
 print('Distance: {}, Speed Increment: {}, Time Variation: {}'.format(d, s, t))
-print('Actual: {}km/hr, Virtual: {}km/hr'.format(s1, s2))
+print('Actual Answer: {}km/hr, Negative Answer: {}km/hr'.format(s1, s2))
 print('A = 1, B = {}, C = -{}'.format(s, c))
 
 print("A train travels {} km. If speed increases by {} kmph, the journey takes {} hours less. Find the original speed!".format(d, s, t))
